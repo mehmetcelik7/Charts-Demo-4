12f472baa756cd89758ba4bdd8eda4e85a091d21
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-05 19:06:31 +0300

    Initial Commit

---
+++ Charts Demo 4/Charts_Demo_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Charts_Demo_4App.swift
+//  Charts Demo 4
+//
+//  Created by mehmet Çelik on 5.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Charts_Demo_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
