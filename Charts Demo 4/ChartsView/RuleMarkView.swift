2a16cfb6ca0107a66c224a8e38df69083229f1b6
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-06 21:36:38 +0300

    değişiyoruz

---
+++ Charts Demo 4/ChartsView/RuleMarkView.swift
@@ -0,0 +1,18 @@
+//
+//  RuleMarkView.swift
+//  Charts Demo 4
+//
+//  Created by mehmet Çelik on 6.03.2025.
+//
+
+import SwiftUI
+
+struct RuleMarkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RuleMarkView()
+}
---
+++ Charts Demo 4/Data/ChartItem.swift
@@ -0,0 +1,8 @@
+//
+//  ChartItem.swift
+//  Charts Demo 4
+//
+//  Created by mehmet Çelik on 6.03.2025.
+//
+
+import Foundation
